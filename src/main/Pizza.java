722d23d462b1fdd3188e0883e0174195531443e4
Sofiane Lasoa <sofiane.lasoa.etu@univ-lille.fr>
2023-09-05 11:19:41 +0200

    Implement Pizza

--- src/main/Pizza.java
+++ src/main/Pizza.java
@@ -1,10 +1,38 @@
 package main;
 
-public class Pizza extends Aliments{
-    
-    private String name;
+import java.util.ArrayList;
 
-    public Pizza(String name) {
-        super(name);
+public enum Pizza{
+
+    SPICY_TEXAS("spicy texas", Aliments.BOEAUF, Aliments.CHORIZZO, Aliments.MOZZARELLA, null, null);
+
+    private String name;
+    private ArrayList<Aliments> AlimentsList = new ArrayList<Aliments>();
+
+    private Pizza(String name, Aliments alim1, Aliments alim2, Aliments alim3, Aliments alim4, Aliments alim5){
+        this.name = name;
+        this.AlimentsList.add(alim1);
+        this.AlimentsList.add(alim2);
+        this.AlimentsList.add(alim3);
+        this.AlimentsList.add(alim4);
+        this.AlimentsList.add(alim5);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public ArrayList<Aliments> getAlimentsList(){
+        return this.AlimentsList;
+    }
+
+    public boolean verifPizza(ArrayList<Aliments> pizzaPerso){
+        boolean isGood = true;
+        for(int i = 0; i < this.AlimentsList.size(); i++){
+            if(!pizzaPerso.contains(this.AlimentsList.get(i))){
+                isGood = false;
+            }
+        }
+        return isGood;
     }
 }
